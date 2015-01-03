74d9ccf77a80586b2b76ab152eb578ed134384ee
Arrow Rowe <arrowrowe@gmail.com>
2015-01-03 18:52:40 +0800

    Change indention and brighten text

--- ai_sway.py
+++ ai_sway.py
@@ -1,14 +1,14 @@
 class Gomoku:
-	def __init__(self, n, index, name='sway'):
-		self.n = n
-		self.index = index
-		self.name = name
-		self.k = -1
+    def __init__(self, n, index, name='sway'):
+        self.n = n
+        self.index = index
+        self.name = name
+        self.k = -1
 
-	def receive(self, x, y):
-		self.k += 1
-		return self.k, 0
+    def receive(self, x, y):
+        self.k += 1
+        return self.k, 0
 
-	def start(self):
-		self.k = 0
-		return 0, 0
+    def start(self):
+        self.k = 0
+        return 0, 0
