53845903987a9380dd0d62db05577d047817ea71
Arrow Rowe <arrowrowe@gmail.com>
2015-01-03 21:40:52 +0800

    ai0: 简易杀手判定

--- ai_arrowrowe.py
+++ ai_arrowrowe.py
@@ -13,9 +13,9 @@ class Gomoku:
             for k in xrange(2)
         ]
         # 记录落子即胜的点
-        self.onekill = [[], []]
-        # 记录落子就会产生 onekill 的点
-        self.tokill = [[], []]
+        self.has_four = [[], []]
+        # 记录落子就会产生 has_four 的点
+        self.has_three = [[], []]
         # 评分常数
         self.value_constants = [0, 1, 2, 12, 32, 64, 128]
 
@@ -31,7 +31,7 @@ class Gomoku:
         tn = 0
         while 0 <= tx < self.n and 0 <= ty < self.n:
             if self.board[tx][ty] != self.board[x][y]:
-                return tn, tx, ty, self.board_get(tx + dx, ty + dy) == self.board[x][y]
+                return tn, tx, ty, self.board[tx][ty] is None and self.board_get(tx + dx, ty + dy) == self.board[x][y]
             tx += dx
             ty += dy
             tn += 1
@@ -53,21 +53,24 @@ class Gomoku:
             if fit(vp, up):
                 self.think[index][vx][vy] += self.value_constants[s + 1] - self.value_constants[s]
             if s >= 4:
-                if ux is not None:
-                    self.onekill[index].append((ux, uy))
-                if vx is not None:
-                    self.onekill[index].append((vx, vy))
+                if self.board_get(ux, uy) is None:
+                    self.has_four[index].append((ux, uy))
+                if self.board_get(vx, vy) is None:
+                    self.has_four[index].append((vx, vy))
             elif s == 3:
                 if self.board_get(ux, uy) is None and self.board_get(vx, vy) is None:
-                    self.tokill[index].append(
+                    self.has_three[index].append(
                         (
                             (ux, uy), (vx, vy)
                         )
                     )
                 if fit(up, vp):
-                    self.onekill[index].append((ux, uy))
+                    self.has_four[index].append((ux, uy))
                 if fit(vp, up):
-                    self.onekill[index].append((vx, vy))
+                    self.has_four[index].append((vx, vy))
+        if (x, y) in self.has_four[1 - index]:
+            self.has_four[1 - index].remove((x, y))
+        self.has_three[1 - index] = filter(lambda three: three[0] != (x, y) and three[1] != (x, y), self.has_three[1 - index])
 
     def value(self, i, j):
         return self.think[self.index][i][j] * 1 + self.think[1 - self.index][i][j] * 2
@@ -87,7 +90,16 @@ class Gomoku:
 
     def receive(self, x, y):
         self.put(x, y, 1 - self.index)
-        i, j = self.find_max()
+        if len(self.has_four[self.index]) > 0:
+            i, j = self.has_four[self.index][0]
+        elif len(self.has_four[1 - self.index]) > 1:
+            return None
+        elif len(self.has_four[1 - self.index]) == 1:
+            i, j = self.has_four[1 - self.index][0]
+        elif len(self.has_three[self.index]) > 0:
+            i, j = self.has_three[self.index][0][0]
+        else:
+            i, j = self.find_max()
         self.put(i, j, self.index)
         return i, j
 
